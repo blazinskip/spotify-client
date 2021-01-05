6bca2ccb92db77a4bcc19d29082664c7edc39138
blazinskip <blazinski.patryk@gmail.com>
2021-01-05 16:02:55 +0100

    feat: 🎸 Add previous & next button in the top bar

--- src/components/TheTopBar.vue
+++ src/components/TheTopBar.vue
@@ -1,7 +1,49 @@
 <template>
   <div
-    class="sticky top-0 z-10 flex-shrink-0 w-full h-16 bg-gray-200 bg-opacity-50 dark:bg-transparent"
-  ></div>
+    class="sticky top-0 z-10 flex-shrink-0 w-full bg-gray-200 bg-opacity-50 h-15 dark:bg-transparent"
+  >
+    <div class="flex items-center h-full px-8">
+      <div class="flex space-x-4">
+        <button
+          class="w-8 h-8 p-1 text-gray-400 bg-black rounded-full cursor-not-allowed bg-opacity-40"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="square"
+              stroke-linejoin="square"
+              stroke-width="1.5"
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+        </button>
+        <button
+          class="w-8 h-8 p-1 text-gray-400 bg-black rounded-full cursor-not-allowed bg-opacity-40"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="square"
+              stroke-linejoin="square"
+              stroke-width="1.5"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </button>
+      </div>
+      <div>
+        <button></button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
-const colors = require("tailwindcss/colors");
-const { scale } = require("tailwindcss/defaultTheme");
-const theme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors');
+const theme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: "class",
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     colors: {
       ...theme.colors,
       gray: colors.trueGray,
     },
     extend: {
+      height: {
+        15: '3.75rem',
+      },
       scale: {
-        "-1": "-1",
+        '-1': '-1',
       },
     },
   },
