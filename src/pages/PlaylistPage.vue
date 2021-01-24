9cb8b39efb14c6c4f3373e888be7465328bb8286
blazinskip <blazinski.patryk@gmail.com>
2021-01-24 12:29:47 +0100

    feat: 🎸 Connect PlaylistPage to router

---
+++ src/pages/PlaylistPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="px-8 py-6">
+    <div style="max-height: 500px; min-height: 340px; height: 30vh">
+      <h2 class="text-gray-50">{{ route.params.playlistId }}</h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+</script>
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,4 @@
-import {
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import SearchPage from './pages/SearchPage.vue';
 import LibraryPage from './pages/LibraryPage.vue';
@@ -10,6 +6,7 @@ import GenrePage from './pages/GenrePage.vue';
 import LoginPage from './pages/LoginPage.vue';
 import AuthenticatedPage from './pages/AuthenticatedPage.vue';
 import LoginSuccessPage from './pages/LoginSuccessPage.vue';
+import PlaylistPage from './pages/PlaylistPage.vue';
 
 const routes = [
   {
@@ -19,6 +16,7 @@ const routes = [
       { path: '/', component: HomePage },
       { path: '/search', component: SearchPage },
       { path: '/library', component: LibraryPage },
+      { path: '/playlist/:playlistId', component: PlaylistPage },
       { path: '/genre/:genreName', component: GenrePage },
     ],
   },
