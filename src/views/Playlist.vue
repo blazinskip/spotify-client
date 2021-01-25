d220a232aad6b106e8d450551969b1a1c06197ba
blazinskip <blazinski.patryk@gmail.com>
2021-01-25 14:18:57 +0100

    feat: 🎸 Add getPlaylist request at Playlist component

--- src/views/Playlist.vue
+++ src/views/Playlist.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="px-8 py-6">
-    <div style="max-height: 500px; min-height: 340px; height: 30vh">
-      <h2 class="text-gray-50">{{ route.params.playlistId }}</h2>
-    </div>
+  <div v-if="playlist" class="px-8 py-6">
+    <div style="max-height: 500px; min-height: 340px; height: 30vh"></div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+  import { defineComponent, onMounted, ref } from 'vue';
   import { useRoute } from 'vue-router';
+  import { HTTPStatusCode, Playlist } from '../types';
+  import useState from '../use/use-state';
+  import { apiRequest } from '../utils';
 
-  const route = useRoute();
+  export default defineComponent({
+    name: 'Playlist',
+    setup() {
+      const route = useRoute();
+      const { token } = useState();
+      const playlist = ref<Playlist | null>(null);
+      onMounted(async () => {
+        if (token && route.params.playlistId) {
+          const { result, status } = await apiRequest<Playlist>(
+            `https://api.spotify.com/v1/playlists/${route.params.playlistId}`
+          );
+
+          if (status === HTTPStatusCode.OK) {
+            playlist.value = result;
+          }
+        }
+      });
+
+      return { route, playlist };
+    },
+  });
 </script>
